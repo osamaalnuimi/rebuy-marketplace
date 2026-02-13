0d76e38c320610e1103fc3037f8dca5fd6e9bfa3
Osamah Alnuaimi <osamaalnuimi@gmail.com>
2026-02-14 00:38:52 +0300

    feat(eslint): refactor ESLint configuration for improved TypeScript support and add ignores

--- eslint.config.js
+++ eslint.config.js
@@ -4,16 +4,33 @@ const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
-module.exports = tseslint.config(
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**', '.angular/**'],
+  },
   {
     files: ['**/*.ts'],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
-      eslintPluginPrettierRecommended,
-    ],
+    ...eslint.configs.recommended,
+  },
+  ...tseslint.configs.recommended.map((config) => ({
+    ...config,
+    files: ['**/*.ts'],
+  })),
+  ...tseslint.configs.stylistic.map((config) => ({
+    ...config,
+    files: ['**/*.ts'],
+  })),
+  ...angular.configs.tsRecommended.map((config) => ({
+    ...config,
+    files: ['**/*.ts'],
+  })),
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
     processor: angular.processInlineTemplates,
     rules: {
       '@angular-eslint/directive-selector': [
@@ -39,13 +56,17 @@ module.exports = tseslint.config(
       ],
     },
   },
+  ...angular.configs.templateRecommended.map((config) => ({
+    ...config,
+    files: ['**/*.html'],
+  })),
+  ...angular.configs.templateAccessibility.map((config) => ({
+    ...config,
+    files: ['**/*.html'],
+  })),
   {
     files: ['**/*.html'],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-      eslintPluginPrettierRecommended,
-    ],
     rules: {},
   },
-);
+  eslintPluginPrettierRecommended,
+];
